825bfffd6c5dd60e4b0cdfcc51500460922c0a93
Shorena Nemsadze <shorenanemsadze96@gmail.com>
2023-11-23 06:21:15 -0800

    Added database changes and migrations.

---
+++ LeaveManagment.Web/Data/LeaveAllocation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagment.Web.Data
+{
+    public class LeaveAllocation
+    {
+        public int Id { get; set; }
+        public int NumberOfDays { get; set; }
+
+        [ForeignKey("LeaveTypeId")]
+        public LeaveType LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        
+        public string EmployeeId { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+    }
+}
+
---
+++ LeaveManagment.Web/Data/LeaveType.cs
@@ -0,0 +1,11 @@
+﻿namespace LeaveManagment.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id {get; set;}
+        public string Name { get; set;}
+        public int DefaultDays { get; set;}
+        public DateTime DateCreated { get; set;}
+        public DateTime DateModified { get; set;}
+    }
+}
