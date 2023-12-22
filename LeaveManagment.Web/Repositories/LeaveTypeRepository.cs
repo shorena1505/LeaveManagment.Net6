34d372138dcecf69a69ead24494a83b9d49e4b38
shorena1505 <shorenanemsadze96@gmail.com>
2023-12-22 20:01:39 +0400

    hell

---
+++ LeaveManagment.Web/Repositories/LeaveTypeRepository.cs
@@ -0,0 +1,12 @@
+﻿using LeaveManagment.Web.Contracts;
+using LeaveManagment.Web.Data;
+
+namespace LeaveManagment.Web.Repositories
+{
+    public class LeaveTypeRepository : GenericRepository<LeaveType>, ILeaveTypeRepository
+    {
+        public LeaveTypeRepository(ApplicationDbContext context) : base(context)
+        {
+        }
+    }
+}
